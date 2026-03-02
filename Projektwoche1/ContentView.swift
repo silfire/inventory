c2979be7c3bdabd599d251031b45d6dfdb715ff4
silfire <ingo.kasprzak@me.com>
2026-03-02 10:35:22 +0100

    Added simple view

--- Projektwoche1/ContentView.swift
+++ Projektwoche1/ContentView.swift
@@ -6,19 +6,36 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+	@Environment(\.modelContext) var modelContext
+
+	@Query() var inventories : [Inventory]
+
+	var body: some View {
+        NavigationStack {
+			List {
+				ForEach(inventories) { inventory in
+					Text(inventory.title)
+				}
+			}
         }
         .padding()
+
+		.onAppear {
+			guard inventories.isEmpty else { return }
+			modelContext.insert(Inventory(title: "Harry Potter 1", count: 1))
+			modelContext.insert(Inventory(title: "Harry Potter 2", count: 1))
+			modelContext.insert(Inventory(title: "Harry Potter 3", count: 1))
+			modelContext.insert(Inventory(title: "Harry Potter 4", count: 1))
+		}
+
     }
 }
 
 #Preview {
+
     ContentView()
+		.modelContainer(for: [Inventory.self], inMemory: true)
 }
