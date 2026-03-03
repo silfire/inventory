db0390711ad97418944d43719c9eec3996efea53
Nicolas Geyer <nicolasgeyer@gmx.de>
2026-03-03 13:07:52 +0100

    Datepicker

--- Projektwoche1/Views/AddItemView.swift
+++ Projektwoche1/Views/AddItemView.swift
@@ -9,54 +9,64 @@ import SwiftUI
 import SwiftData
 
 struct AddItemView: View {
-	@Environment(\.dismiss) var dismiss
-	@Environment(\.modelContext) var context
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) var context
     
     @Query var itemTypes: [ItemType]
-
-	@State private var items : Item = Item(title: "", count: 1)
-	@State private var sliderValue : Double = 1
     
+    @State private var items : Item = Item(title: "", count: 1)
+    @State private var sliderValue : Double = 1
+    
+    @State private var selectedDate: Date = .now
     @State private var selectedCategory : ItemType?
     
     var body: some View {
-		Form {
-			TextField("Name", text: $items.title)
+        Form {
+            TextField("Füge ein neues Item hinzu", text: $items.title)
             
-            Picker("Kategorie", selection: $selectedCategory) {
+            Picker("Kategorie", selection: $items.type) {
                 ForEach(itemTypes) { category in
-                    Text(category.title).tag(category)
+                    Text(category.title)
+                        .tag(category)
+                }
+            }
+            .onAppear {
+                if items.type == nil {
+                    items.type = itemTypes.first
                 }
             }
             
-			VStack {
-				Text("Anzahl")
-				HStack {
-					Text("0")
-					Slider(value: $sliderValue, in: 0...50, step: 1, label: {
-						Text("Anzahl")
-					})
-					Text("50")
-				}
-				Text("\(Int(sliderValue))")
-			}
-			Section {
-				Button {
-					items.count = Int(sliderValue)
-					context.insert(items)
-					dismiss()
-				} label: {
-					Text("Hinzufügen")
-				}
-			}
-		}
-		.navigationTitle("New item")
+            DatePicker("Datum auswählen", selection: $items.date, displayedComponents: .date)
+            
+            VStack {
+                Text("Anzahl")
+                HStack {
+                    Text("0")
+                    Slider(value: $sliderValue, in: 0...50, step: 1, label: {
+                        Text("Anzahl")
+                    })
+                    Text("50")
+                }
+                Text("\(Int(sliderValue))")
+                
+                Section {
+                    Button {
+                        items.count = Int(sliderValue)
+                        items.date = selectedDate 
+                        context.insert(items)
+                        dismiss()
+                    } label: {
+                        Text("Hinzufügen")
+                    }
+                }
+            }
+            .navigationTitle("New item")
+        }
     }
 }
-
 #Preview {
-	NavigationStack {
-		AddItemView()
-			.modelContainer(DataProvider.preview.container)
-	}
+    NavigationStack {
+        AddItemView()
+            .modelContainer(DataProvider.preview.container)
+    }
 }
