58554c51ec90f5157531f5d715743d5a5ae2fdf6
KCorigliano <corigliano98@hotmail.com>
2026-03-03 15:33:53 +0100

    imagePicker in itemDetailView

--- Projektwoche1/Views/ItemDetailView.swift
+++ Projektwoche1/Views/ItemDetailView.swift
@@ -7,11 +7,15 @@
 
 import SwiftUI
 import SwiftData
+import PhotosUI
 
 struct ItemDetailView: View {
 	@Environment(\.modelContext) var context
 
     @State var item : Item = Item(title: "", count: 1)
+    
+    @State private var selectedImage : PhotosPickerItem? = nil
+    @State private var uiImage : UIImage? = nil
     
     // TODO: ItemType verbinden
     @State var itemCategory: String = "Books"
@@ -26,6 +30,30 @@ struct ItemDetailView: View {
                 .font(.largeTitle)
                 .bold(true)
             Form {
+                Section {
+                    HStack {
+                        ZStack (alignment: .topTrailing) {
+                            Group {
+                                if let uiImage = uiImage {
+                                    Image(uiImage: uiImage)
+                                        .resizable()
+                                        .scaledToFit()
+                                } else {
+                                    Image(systemName: "photo.fill")
+                                        .resizable()
+                                        .foregroundStyle(.gray)
+                                }
+                            }
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .overlay(
+                                PhotosPicker(selection: $selectedImage, matching: .images) {
+                                }
+                            )
+                        }
+                    }
+                }
+                
                 TextField("Title", text: $item.title)
                 Stepper(value: $item.count, in: 0...100) {
                     Text("Quantity: \(item.count)")
@@ -34,6 +62,22 @@ struct ItemDetailView: View {
                     ForEach(categories, id: \.self) {
                         category in Text(category).tag(category)
                     }
+                }
+            }
+            .onChange(of: selectedImage) { oldItem, newImage in
+                Task {
+                    guard let newImage else { return }
+                    
+                    do {
+                        if let data = try? await newImage.loadTransferable(type: Data.self) {
+                            if let image = UIImage(data: data) {
+                                uiImage = image
+                            }
+                        }
+                    } catch {
+                        print(error)
+                    }
+
                 }
             }
         }
