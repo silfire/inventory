232c2d38506b3aeb2c27183912635c69da44a575
silfire <ingo.kasprzak@me.com>
2026-03-05 10:29:37 +0100

    Debugged AddItemView

--- Projektwoche1/Views/AddItemView.swift
+++ Projektwoche1/Views/AddItemView.swift
@@ -9,64 +9,71 @@ import SwiftUI
 import SwiftData
 
 struct AddItemView: View {
-    @Environment(\.dismiss) var dismiss
-    @Environment(\.modelContext) var context
-    
-    @Query var itemTypes: [Category]
-    
-    @State private var items : Item = Item(title: "", count: 1)
-    @State private var sliderValue : Double = 1
-    
-    @State private var selectedDate: Date = .now
-    @State private var selectedCategory : Category?
-    
-    var body: some View {
-        Form {
-            TextField("Füge ein neues Item hinzu", text: $items.title)
-            
-            Picker("Kategorie", selection: $items.category) {
-                ForEach(itemTypes) { category in
-                    Text(category.title)
-                        .tag(category)
-                }
-            }
-            .onAppear {
-                if items.category == nil {
-                    items.category = itemTypes.first
-                }
-            }
-            
-            DatePicker("Datum auswählen", selection: $items.date, displayedComponents: .date)
-            
-            VStack {
-                Text("Anzahl")
-                HStack {
-                    Text("0")
-                    Slider(value: $sliderValue, in: 0...50, step: 1, label: {
-                        Text("Anzahl")
-                    })
-                    Text("50")
-                }
-                Text("\(Int(sliderValue))")
-                
-                Section {
-                    Button {
-                        items.quantity = Int(sliderValue)
-                        items.date = selectedDate 
-                        context.insert(items)
-                        dismiss()
-                    } label: {
-                        Text("Hinzufügen")
-                    }
-                }
-            }
-            .navigationTitle("New item")
-        }
-    }
+	@Environment(\.dismiss) var dismiss
+	@Environment(\.modelContext) var context
+
+	@Query(sort: \Category.title) var itemTypes: [Category]
+
+	@State private var title : String = ""
+	@State private var quantity : Int = 1
+
+	@State private var selectedDate: Date = .now
+	@State private var selectedCategoryId : PersistentIdentifier?
+
+	var body: some View {
+		ZStack {
+			Form {
+				TextField("Titel", text: $title)
+
+				Picker("Kategorie", selection: $selectedCategoryId) {
+					ForEach(itemTypes, id: \.id) { category in
+						Text(category.title)
+							.tag(category.id)
+					}
+				}
+				.onAppear {
+					if selectedCategoryId == nil {
+						selectedCategoryId = itemTypes.first?.id
+					}
+				}
+
+				DatePicker("Datum auswählen", selection: $selectedDate, displayedComponents: .date)
+
+				Stepper(value: $quantity, in: 0...1000) {
+					HStack {
+						Text("Anzahl")
+						Spacer()
+						Text("\(quantity)")
+					}
+				}
+
+				Section {} footer: {
+					Spacer().frame(height: 20)
+				}
+			}
+			VStack {
+				Spacer()
+				Button {
+					let category = itemTypes.first { $0.id == selectedCategoryId }
+					let item = Item(title: title,
+									count: quantity,
+									category: category)
+					item.date = selectedDate
+					context.insert(item)
+					dismiss()
+				} label: {
+					Label("Hinzufügen", systemImage: "plus")
+				}
+				.controlSize(.large)
+				.buttonStyle(.bordered)
+			}
+		}
+		.navigationTitle("New item")
+	}
 }
 #Preview {
-    NavigationStack {
-        AddItemView()
-            .modelContainer(DataProvider.preview.container)
-    }
+	NavigationStack {
+		AddItemView()
+			.modelContainer(DataProvider.preview.container)
+	}
 }
