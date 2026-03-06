3294bac43a3b3676ccc2f09f80ad997f9386c509
silfire <ingo.kasprzak@me.com>
2026-03-06 16:05:16 +0100

    Added DataProvider to App struct

--- Projektwoche1/Projektwoche1App.swift
+++ Projektwoche1/Projektwoche1App.swift
@@ -14,6 +14,6 @@ struct Projektwoche1App: App {
         WindowGroup {
             ContentView()
         }
-		.modelContainer(for: [Item.self], inMemory: true)
+		.modelContainer(DataProvider.shared.container)
     }
 }
